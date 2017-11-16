b4681d2f0b23cb7fc4d6edf9526df11d0cacb54a
Oleksandr <litvinov.rd@gmail.com>
2017-11-16 22:17:11 +0200

    Arduino USB Reader with Dot Net Core 2.0 Console Application

---
+++ ArduinoReader/ArduinoReader/Program.cs
@@ -0,0 +1,49 @@
+﻿using ReadSerialInputFromUSB.Example;
+using System;
+using System.Linq;
+
+namespace ArduinoReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            try
+            {
+                StartComunication();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void StartComunication()
+        {
+            var ports = SerialInformation.GetPorts();
+            if (ports.Any())
+            {
+                Console.WriteLine($"Found {ports.Count()} ports: " + ports.Aggregate((x, y) => $"{x} {y}"));
+                var port = ports.First();
+
+                Console.WriteLine($"Connecting to {port}");
+
+                var serialInformation = new SerialInformation(port);
+
+                serialInformation.ReadFromPort();
+
+                Console.ReadKey();
+
+                serialInformation.SerialPort.Close();
+            }
+            else
+            {
+                Console.WriteLine("No ports was found");
+            }
+        }
+    }
+}
---
+++ ArduinoReader/ReadSerialInputFromUSB/Example/SerialInformation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+
+namespace ReadSerialInputFromUSB.Example
+{
+    public class SerialInformation
+    {
+        public SerialPort SerialPort { get; set; }
+        private string SerialPortName { get; }
+
+        public SerialInformation(string serialPortName)
+        {
+            SerialPortName = serialPortName;
+        }
+
+        public static IEnumerable<string> GetPorts()
+        {
+            return SerialPort.GetPortNames();            
+        }
+
+        public void ReadFromPort()
+        {
+            // Initialise the serial port on COM4.
+            // obviously we would normally parameterise this, but
+            // this is for demonstration purposes only.
+            this.SerialPort = new SerialPort(SerialPortName)
+            {
+                BaudRate = 9600,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                DataBits = 8,
+                Handshake = Handshake.None
+            };
+
+            // Subscribe to the DataReceived event.
+            this.SerialPort.DataReceived += SerialPortDataReceived;
+
+            // Now open the port.
+            this.SerialPort.Open();
+        }
+
+        private void SerialPortDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var serialPort = (SerialPort)sender;
+
+            // Read the data that's in the serial buffer.
+            var serialdata = serialPort.ReadExisting();
+
+            // Write to debug output.
+            Debug.Write(serialdata);
+
+            Console.Write(serialdata);
+        }
+    }
+}
